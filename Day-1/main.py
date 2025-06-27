6b4d0ef30eb0cdddbde9c58911a6d678e05a225d
not-developer-exe <official.additya.singh@gmail.com>
2025-06-27 19:09:01 +0530

    Day-0 of Learning Data Science

---
+++ Day-1/main.py
@@ -0,0 +1,71 @@
+# First code
+# print('Hello World!')
+
+# Variables
+# Write anything, add '=',it becomes variable in Python
+
+# abc = 10
+
+# Ways to declare variables
+
+# adityaSingh = 10 # Camel Case
+# adityasingh = 10 # Snake Case
+# AdityaSingh = 10 # Pascal Case 
+
+
+# Data Types
+#refer notes
+
+# Strings and Conversion
+
+# a = 'A'
+# print(ord(a))
+
+# a = 65
+# print(chr(a))
+
+#String Slicing
+
+# a = 'Aditya KODR'
+# print(a[4::1])
+
+# Type Conversion
+#a = 10
+# print(type(a))
+
+#a = str(a) # Leooo, int bnn gya str
+#print(type(a))
+
+# age = int(input("Enter your age: "))
+#print(type(age))
+#print(f"My age is {age}")
+
+# Operators
+# Arithmetic, comparison, logical, and much more
+#print(1+2) #Addition
+#print(2-1) #Sub
+#print(2*3) #Multiply
+#print(12/4) #Division
+#print(12//4) #Floor Division
+#print(2**5) #Exponential
+
+# Assignment Operator
+# Assigns value to any variable
+
+# Compound Assignment Operations
+
+#a = 20
+#a += 20
+#a += 40
+#a += 60
+#print(a)
+
+# Comparison Operator
+# == : Equals to
+# != : Not equals to
+# < : Less Than
+# > : Greater Than
+# <= : Less than Equals to
+# >= : Greater than Equals to
+
+#Logical Operators
